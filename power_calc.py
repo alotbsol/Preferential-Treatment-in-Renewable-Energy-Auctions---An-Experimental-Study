b5db84c3db81385e52000115a6f612bec2f2f02a
alotbsol <prokop.cech@seznam.cz>
2023-04-12 09:01:47 +0200

    power_calc

--- power_calc.py
+++ power_calc.py
@@ -2,7 +2,9 @@ from random_parameters_generation import generate_random_parameters
 from theory_calc import AuctionGeneratorTheory
 
 import pandas as pd
+import random
 from scipy.stats import ttest_ind
+from math import floor
 
 
 def play_scenario(parameters_df, demand=1, rym=0,):
@@ -82,7 +84,102 @@ def power_calc(rounds_per_treatment=50):
     writer.save()
 
 
+def power_calc_samples(rounds_per_treatment=50):
+    writer = pd.ExcelWriter("distributions/{0}_samples_rounds_{rounds}.xlsx".format("power_calc",
+                                                                          rounds=str(rounds_per_treatment)),
+                            engine="xlsxwriter")
+
+    for demand in [3, 1]:
+        average_subsidy_storage = {"average_subsidy_no_rym": [],
+                                   "average_subsidy_rym": [],
+                                   }
+
+        for iteration in range(0, 500):
+            print("sampling", iteration + 1, "out of 500")
+            rand_gen = random.Random()
+            random_seed = rand_gen.randint(1, 1000000)
+
+            scenario_parameters = generate_random_parameters(no_groups=1, plyers_per_group=4,
+                                                             no_rounds=rounds_per_treatment,
+                                                             ws_min=5, ws_max=9,
+                                                             oc_min=0.8, oc_max=1.2,
+                                                             ws_decimals=1,
+                                                             oc_decimals=2,
+                                                             base_lcoe=50,
+                                                             max_bid_no_rym=90.3,
+                                                             max_bid_rym=70,
+                                                             random_seed=random_seed,
+                                                             name="scenario_x",
+                                                             )
+
+            results = (play_scenario(parameters_df=scenario_parameters, demand=demand))
+
+            results_subsidy_no_rym = sum(results["1"]["NoRYM_model_subsidy"])/len(results["1"]["NoRYM_model_subsidy"])
+            results_subsidy_rym = sum(results["1"]["RYM_model_subsidy"])/len(results["1"]["RYM_model_subsidy"])
+
+            average_subsidy_storage["average_subsidy_no_rym"].append(results_subsidy_no_rym)
+            average_subsidy_storage["average_subsidy_rym"].append(results_subsidy_rym)
+
+        print(average_subsidy_storage)
+
+        storage_length = len(average_subsidy_storage["average_subsidy_rym"])
+
+        power_results_subsidy = {}
+
+        for samples_needed in range(2, 7):
+            power_results_subsidy[str(samples_needed)] = {
+                                                         "t": [],
+                                                         "p": [],
+                                                         "average_subsidy_no_rym": [],
+                                                         "average_subsidy_rym": [],
+                                                         }
+
+            samples_fits_in_storage_x = floor(storage_length/samples_needed)
+            sample_repetition = 1
+
+            for i in range(0, samples_fits_in_storage_x):
+                sample_start = samples_needed * (sample_repetition - 1)
+                sample_end = samples_needed * (sample_repetition - 1) + samples_needed
+
+                sample_subsidy_no_rym = average_subsidy_storage["average_subsidy_no_rym"][sample_start:sample_end]
+                sample_subsidy_rym = average_subsidy_storage["average_subsidy_rym"][sample_start:sample_end]
+
+                t_stat, p_value = ttest_ind(sample_subsidy_no_rym, sample_subsidy_rym)
+
+                power_results_subsidy[str(samples_needed)]["t"].append(t_stat)
+                power_results_subsidy[str(samples_needed)]["p"].append(p_value)
+
+                power_results_subsidy[str(samples_needed)]["average_subsidy_no_rym"].\
+                    append(sum(sample_subsidy_no_rym) / len(sample_subsidy_no_rym))
+
+                power_results_subsidy[str(samples_needed)]["average_subsidy_rym"].\
+                    append(sum(sample_subsidy_rym) / len(sample_subsidy_rym))
+
+                sample_repetition += 1
+
+        print("power_results", power_results_subsidy)
+
+        for samples in power_results_subsidy:
+            df_out = pd.DataFrame.from_dict(power_results_subsidy[samples])
+            df_out.to_excel(writer, sheet_name=("demand_"+str(demand)) + "_sample_" + str(samples))
+
+    writer.save()
+
+
+def t_test_check():
+    set1 = [59.5595441850447, 59.9654016279055]
+    set2 = [52.6727339733269, 53.2910663553892]
+
+    t_stat, p_val = ttest_ind(set1, set2, equal_var=False)
+
+    print("t-statistic:", t_stat)
+    print("p-value:", p_val)
+
+
 if __name__ == '__main__':
-    power_calc(rounds_per_treatment=25)
-    power_calc(rounds_per_treatment=50)
+    # power_calc(rounds_per_treatment=25)
+    # power_calc(rounds_per_treatment=50)
+
+    for i in [25, 50]:
+        power_calc_samples(rounds_per_treatment=i)
 
